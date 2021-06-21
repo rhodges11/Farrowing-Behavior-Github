4e88a8a2f687c70bd1caafae7e2d26c6d1212e2e
Bryanna Fayne <bfayne@ncsu.edu>
2021-06-21 09:49:27 -0500

    Combined files and wrote to a csv file.

---
+++ code/sow_data.R
@@ -0,0 +1,18 @@
+#############
+# Author: Bryanna Fayne
+# Date: June 21st, 2021
+# Combining csv files and analyzing behavior summary counts 
+
+# Load packages 
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+
+# Read in csv files 
+a <- read_csv("sow_behavior_sum_1_10.csv")
+b <- read_csv("sow_behavior_sum_11_20.csv")
+
+# Bind files and write to new file 
+behavior_counts <- bind_rows(a, b)
+
+write_csv(behavior_counts, "behavior_counts.csv")
